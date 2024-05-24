41d981bf2182edc69a7373b32173fbeb9013a53e
Thomas Dixini <thomasdixini@gmail.com>
2024-05-23 21:07:08 -0300

    Criando e manipulando Links (Symbolic Links)

--- IoStudy.java
+++ IoStudy.java
@@ -2,10 +2,12 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.CopyOption;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileStore;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
@@ -14,7 +16,7 @@ import java.nio.file.StandardCopyOption;
  * IoStudy
  */
 public class IoStudy {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, NoSuchFileException, FileAlreadyExistsException {
 
         /*File file = new File("C:\\Users\\ThomásDixini\\Documents\\Dev\\afundo_java\\diretorio_teste");
         System.out.printf(
@@ -35,7 +37,7 @@ public class IoStudy {
             System.out.println(store.name() + " - " + store.type());
         }
         */
-        try {
+        /*try {
             Path path = Paths.get("C:\\Users\\ThomásDixini\\Documents\\Dev\\afundo_java\\directory\\");
             path.toFile().mkdir();
             path = Paths.get("C:\\Users\\ThomásDixini\\Documents\\Dev\\afundo_java\\directory\\teste.txt");
@@ -55,6 +57,10 @@ public class IoStudy {
             
         } catch (Exception e) {
             System.err.println(e);
-        }
+        }*/
+        Path link = Paths.get("C:\\Users\\ThomásDixini\\Documents\\Dev\\afundo_java\\link");
+        Path target = Paths.get("C:\\Users\\ThomásDixini\\Documents\\Dev\\afundo_java\\directory\\arquivo2.txt");
+        Files.createSymbolicLink(link, target);
+        System.out.println("É um link simbolico: " + Files.isSymbolicLink(link) + " e seu arquivo é: " + Files.readSymbolicLink(link));
     }
 }
